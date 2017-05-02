52b0d4f0c588920ffc39b668692c3005689d4456
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-05-02 22:48:43 +0200

    Commit

--- NipGeneratorMap/NipGeneratorMap/Classes/KonwerterWysokosciNaZnak.cs
+++ NipGeneratorMap/NipGeneratorMap/Classes/KonwerterWysokosciNaZnak.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NipGeneratorMap.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace NipGeneratorMap.Classes
 {
-    public class KonwerterWysokosciNaZnak
+    public class KonwerterWysokosciNaZnak : Interfaces.IKonwerterWysokosciNaZnak
     {
+        private IDostarczycielZnakowAsci _dostarczycielZnakow;
+
+        public KonwerterWysokosciNaZnak(IDostarczycielZnakowAsci dostarczycielZnakow)
+        {
+            _dostarczycielZnakow = dostarczycielZnakow;
+        }
+        public string WysokoscNaZnak(int wartoscWysokosci)
+        {
+            if(_dostarczycielZnakow.ZnakWysokosci().GetLowerBound(0) <= wartoscWysokosci && _dostarczycielZnakow.ZnakWysokosci().GetUpperBound(0)>= wartoscWysokosci)
+            {
+                return _dostarczycielZnakow.ZnakWysokosci()[wartoscWysokosci];
+            }
+
+            return _dostarczycielZnakow.ZnakiNieokreslonejWysokosci();
+        }
     }
 }
--- NipGeneratorMap/NipGeneratorMap/Program.cs
+++ NipGeneratorMap/NipGeneratorMap/Program.cs
@@ -11,7 +11,10 @@ namespace NipGeneratorMap
     {
         static void Main(string[] args)
         {
-            var unity = new UnityContainer();
+            var container = new UnityContainer();
+
+            container.RegisterType<Interfaces.IGeneratorMapy, Classes.GeneratorKonsolowyMapy>();
+
         }
     }
 }
